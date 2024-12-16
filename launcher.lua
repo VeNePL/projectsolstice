9442c89301ca3e4ff243a763224abfeb9c015cfc
veneepl <65218086+VeNePL@users.noreply.github.com>
2024-12-16 22:06:13 +0100

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -24,13 +24,13 @@ local Window = Rayfield:CreateWindow({
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
  
-    KeySystem = false, -- Set this to true to use our key system
+    KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Untitled",
+       Title = "Project Solstice",
        Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+       Note = "", -- Use this to tell the user how to get a key
        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
        GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
        Key = "https://raw.githubusercontent.com/VeNePL/projectsolstice/refs/heads/main/public_key.txt" -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
     }
